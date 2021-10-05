763126ad17ed51e36a2df009452694b499836f01
dohako <dohako@inbox.ru>
2021-10-05 14:58:59 +0300

    CHG path to script

--- src/git_checker.rs
+++ src/git_checker.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 pub fn start(){
     println!("Start git checker!");
     run_command("chmod +x /home/pi/d_sh_handler_2/src/git_checker/git-repo-watcher");
-    run_command("/home/pi/d_sh_handler_2/src/git_checker/git-repo-watcher");
+    run_command("/home/pi/d_sh_handler_2/src/git_checker/git-repo-watcher -d /home/pi/d_sh_handler");
 }
 
 fn run_command(command:&'static str){
