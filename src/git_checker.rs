589e83d156ede751d3557b5077517e90d1e9c236
dohako <dohako@inbox.ru>
2021-10-05 14:53:57 +0300

    TEST git checker

--- src/git_checker.rs
+++ src/git_checker.rs
@@ -2,14 +2,11 @@ use std::process::Command;
 
 pub fn start(){
     println!("Start git checker!");
-    run_command("chmod +x /home/pi/d_sh_handler_2/git_checker/git-repo-checker");
-    let out = run_command("/home/pi/d_sh_handler_2/git_checker/git-repo-checker");
-    let s = format!("{:?}", &out);
-    println!("{}", s);
-    println!("{:?}", out);
+    run_command("chmod +x /home/pi/d_sh_handler_2/git_checker/git-repo-watcher");
+    run_command("/home/pi/d_sh_handler_2/git_checker/git-repo-watcher");
 }
 
-fn run_command(command:&'static str) -> Vec<u8>{
+fn run_command(command:&'static str){
     let output = if cfg!(target_os = "windows") {
         println!("Windows");
         Command::new("cmd")
@@ -24,9 +21,6 @@ fn run_command(command:&'static str) -> Vec<u8>{
                 .output()
                 .expect("failed to execute process")
     };
-    
+
     println!("{:#?}", output);
-    let _result = output.stdout;
-    let _err_result = output.stderr;
-    return _err_result;
 }
