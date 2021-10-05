702b73f469355de1ff3a48aa4c97ceac765bf6af
dohako <dohako@inbox.ru>
2021-10-05 16:25:12 +0300

    TEST git checker

--- src/git_checker.rs
+++ src/git_checker.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 pub fn start(){
     println!("Start git checker!!!!!!!!!!!!!!");
-    run_command("git checkout ./autorun/d_sh_handler_2");
+    // run_command("git checkout ./autorun/d_sh_handler_2");
     run_command("chmod +x /home/pi/d_sh_handler_2/src/git_checker/git-repo-watcher");
     run_command("/home/pi/d_sh_handler_2/src/git_checker/git-repo-watcher -d /home/pi/d_sh_handler_2");
 }
@@ -17,7 +17,6 @@ fn run_command(command:&'static str){
     } else {
         Command::new("sh")
                 .arg("-c")
-                .arg("cd /home/pi/d_sh_handler_2")
                 .arg(command)
                 .output()
                 .expect("failed to execute process")
